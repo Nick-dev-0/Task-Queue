cb4d276f03087fff0da15f57634d5b360735fae5
Nick <NickLuna.dev@gmail.com>
2026-08-06 13:04:48 -0400

    feat: add producer script to enqueue jobs onto Redis List

---
+++ producer.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const Redis = require("ioredis");
+
+const redis = new Redis(process.env.REDIS_URL);
+
+const sampleJob = { type: "sendEmail", payload: { to: "test@test.com" } };
+
+async function addJobs() {
+  try {
+    const job = await redis.lpush("jobs", JSON.stringify(sampleJob));
+    if (job) console.log("Successfully added job");
+  } catch (error) {
+    console.log("Something went wrong", error);
+  }
+}
+redis.disconnect()
+
+addJobs();
